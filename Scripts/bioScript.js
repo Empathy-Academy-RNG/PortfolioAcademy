d1bce51b79f7d37d3b3fc64faca4aeb9ba5dd845
raulng9 <salprez4@gmail.com>
2021-02-21 01:48:32 +0100

    Added simple animation to bio photo

--- Scripts/bioScript.js
+++ Scripts/bioScript.js
@@ -1,21 +1,19 @@
-//Simulate typewriter style
-let verticalLine = "<div id='bioVerticalLine'></div>";
-
 $(document).ready(function() {
-    simulateSelection();
+    simulateLinking();
+    photoAnimation();
 });
-
-
-function simulateSelection(){
-    $("img.desaturatePhoto").addClass("saturatePhoto");
-    $("a#proyectsLink").css("background-color", "rgb(179,216,253)");
-    setTimeout(simulateLinking, 500);
-}
-
 
 function simulateLinking(){
     $("a#proyectsLink").css("text-decoration", "underline");
     $("a#proyectsLink").css("color", "#2980b9");
-    $("a#proyectsLink").css("font-size", "1.3em");
-    setTimeout(function(){$("a#proyectsLink").css("background-color", "white");},400);
+}
+
+function photoAnimation(){
+    $('.spin').addClass("spinning");
+    setTimeout(1000, animateImageFade());
+}
+
+function animateImageFade(){
+    $("#photoBio").addClass("photoFadeIn");
+    $("#photoBio").addClass("saturatePhoto");
 }
